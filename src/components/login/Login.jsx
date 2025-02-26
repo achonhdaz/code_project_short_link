3c674dd37bf9514d3054060e9f65f7a48bcac03f
achraf chemhki <achrafchemkhi671@gmail.com>
2025-02-27 00:01:36 +0100

    Version 0.02

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+        <h5>
+        A.step one understand the template code 
+    B.do the logn in sgin up code 
+    C. attend and online course about mern 
+        </h5>
+
+    </div>
+  );
+};
+
+export default App;
